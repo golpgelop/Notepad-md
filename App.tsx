a601bbf54b8df2e044acf83ab64ca831c0d12923
golpgelop <golpgelop@gmail.com>
2026-04-07 00:46:37 +0400

    рабочий код

--- App.tsx
+++ App.tsx
@@ -3,7 +3,17 @@ import { View } from 'react-native';
 import { app } from './App.styles';
 import PageManager from './pages/PageManager';
 
+import * as NavigationBar from 'expo-navigation-bar';
+import * as SystemUI from 'expo-system-ui';
+import { useEffect } from 'react';
+
 const App: React.FC = () => {
+
+    // useEffect(() => {
+    //     SystemUI.setBackgroundColorAsync('#1a1a1a');
+
+    //     NavigationBar.setButtonStyleAsync('light');
+    // }, []);
 
     return (
         <View style={app.container}>
