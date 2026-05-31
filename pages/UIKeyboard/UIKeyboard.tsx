fc397e9d13201a5496d6b2c7744bc98bf3dabc44
golpgelop <golpgelop@gmail.com>
2026-05-31 06:14:28 +0400

    мелкая правка

--- pages/UIKeyboard/UIKeyboard.tsx
+++ pages/UIKeyboard/UIKeyboard.tsx
@@ -1,12 +1,22 @@
 import { TouchableOpacity, View, Text } from 'react-native';
 import { ui } from './UIKeyboard.styles';
 
-
 const UIKeyboard: React.FC = () => {
+    const handlePressLeft = () => {};
 
-    const handlePressLeft = () => {
-    }
+    const handlePressRight = () => {};
 
+    const handlePressUp = () => {};
+
+    const handlePressDown = () => {};
+
+    const handlePressPut = () => {};
+
+    const handlePressPast = () => {};
+
+    const handlePressCopy = () => {};
+
+    const handlePressCancel = () => {};
 
     return (
         <View style={ui.container}>
@@ -14,35 +24,35 @@ const UIKeyboard: React.FC = () => {
                 <Text style={ui.ButtonText}>←</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={ui.Button} onPress={handlePressLeft}>
+            <TouchableOpacity style={ui.Button} onPress={handlePressRight}>
                 <Text style={ui.ButtonText}>→</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={ui.Button} onPress={handlePressLeft}>
+            <TouchableOpacity style={ui.Button} onPress={handlePressUp}>
                 <Text style={ui.ButtonText}>↑</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={ui.Button} onPress={handlePressLeft}>
+            <TouchableOpacity style={ui.Button} onPress={handlePressDown}>
                 <Text style={ui.ButtonText}>↓</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={ui.Button} onPress={handlePressLeft}>
+            <TouchableOpacity style={ui.Button} onPress={handlePressPut}>
                 <Text style={ui.ButtonText}>put</Text>  
             </TouchableOpacity>
 
-            <TouchableOpacity style={ui.Button} onPress={handlePressLeft}>
+            <TouchableOpacity style={ui.Button} onPress={handlePressPast}>
                 <Text style={ui.ButtonText}>past</Text>  
             </TouchableOpacity>
 
-            <TouchableOpacity style={ui.Button} onPress={handlePressLeft}>
+            <TouchableOpacity style={ui.Button} onPress={handlePressCopy}>
                 <Text style={ui.ButtonText}>copy</Text>  
             </TouchableOpacity>
 
-            <TouchableOpacity style={ui.Button} onPress={handlePressLeft}>
+            <TouchableOpacity style={ui.Button} onPress={handlePressCancel}>
                 <Text style={ui.ButtonText}>cancel</Text>  
             </TouchableOpacity>
         </View>
     );
-}
+};
 
 export default UIKeyboard;
